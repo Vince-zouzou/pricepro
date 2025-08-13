5b937cdd276e686e8e5e73a026137f4b43507764
YSOMAD <65582466+Vince-zouzou@users.noreply.github.com>
2025-08-14 03:37:23 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+## Set page_config
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+## laod sample data
+data = {
+    "client" : pd.read_excel('resources\data\Copy of 10Jul_DummyData_v1.xlsx',sheet_name='ClientGroup'),
+    "transactions": pd.read_excel('resources\data\Copy of 10Jul_DummyData_v1.xlsx',sheet_name='TransactionData'),
+}
+
+st.session_state['data'] = data
+data['client']['RoA'] = data['client']['Total Relationship Revenue']/data['client']['Average Relationship AuM'].astype(float)
+
+
+pages = [
+    st.Page("pages\\Management.py",title="Client Management")]
+st.session_state['profile'] = st.Page("pages\\Profile.py",title="Client Profile")
+st.logo('resources\images\simon-kucher-seeklogo.png',size = 'large',link = 'https://www.simon-kucher.com')
+nav = st.navigation(pages,position='top')
+
+nav.run()
